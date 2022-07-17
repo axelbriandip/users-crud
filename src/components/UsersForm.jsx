0aa43687116ab2e70aefde17ee3c44a0400f97c9
axelbriandip <axelbriandip.rg@gmail.com>
2022-07-17 20:05:12 -0300

    agregué validación en fecha

--- src/components/UsersForm.jsx
+++ src/components/UsersForm.jsx
@@ -48,26 +48,45 @@ const UsersForm = ({ addUser, userSelected, deselectUser, modifyUser }) => {
         deselectUser();
         reset();
     }
+    const today = new Date(); // fecha hoy
+    // tratar mes
+    let month = "";
+    if((today.getMonth + 1) >= 10) {
+        month = today.getMonth() + 1;
+    } else {
+        month = "0" + (today.getMonth() + 1);
+    }
+    // tratar día
+    let day = "";
+    if(today.getDate() >= 10) {
+        day = today.getDate();
+    } else {
+        day = "0" + today.getDate();
+    }
+    // resto de tratamiento para fecha
+    const year = today.getFullYear();
+    const strToday = `${year}-${month}-${day}`;
+    // console.log("today: " + strToday);
     return (
         <div className='container-form'>
             <form onSubmit={submit}>
                 <h2>New user</h2>
                 <div className="container-input">
-                    <i class="fa-solid fa-user"></i>
+                    <i className="fa-solid fa-user"></i>
                     <input type="text" className='input-fname' placeholder='first name' value={firstName} onChange={e => setFirstName(e.target.value)}/>
                     <input type="text" className='input-lname' placeholder='last name' value={lastName} onChange={e => setLastName(e.target.value)}/>
                 </div>
                 <div className="container-input">
-                    <i class="fa-solid fa-envelope"></i>
+                    <i className="fa-solid fa-envelope"></i>
                     <input type="email" className='input-email' placeholder='email' value={email} onChange={e => setEmail(e.target.value)}/>
                 </div>
                 <div className="container-input">
-                    <i class="fa-solid fa-key"></i>
+                    <i className="fa-solid fa-key"></i>
                     <input type="password" className='input-password' placeholder='password' value={password} onChange={e => setPassword(e.target.value)}/>
                 </div>
                 <div className="container-input">
-                    <i class="fa-solid fa-calendar-days"></i>
-                    <input type="date" className='input-birthday' value={birthday} onChange={e => setBirthday(e.target.value)}/>
+                    <i className="fa-solid fa-calendar-days"></i>
+                    <input type="date" className='input-birthday' max={strToday} value={birthday} onChange={e => setBirthday(e.target.value)}/>
                 </div>
                 <button>{userSelected !== null ? "Modify" : "Create"}</button>
                 {userSelected !== null && <button onClick={clean}>Clean</button>}
--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -10,12 +10,12 @@ const UsersList = ({ users, deleteUser, selectUser }) => {
                             <span className='name'><strong>{user.first_name} {user.last_name}</strong></span>
                             <span className='email'>{user.email}</span>
                             <div className='div-calendar'>
-                                <i class="fa-solid fa-calendar-days calendar-list"></i><span className='birthday'>{user.birthday}</span>
+                                <i className="fa-solid fa-calendar-days calendar-list"></i><span className='birthday'>{user.birthday}</span>
                             </div>
                         </div>
                         <div className="buttons">
-                            <i onClick={() => deleteUser(user.id)} class="fa-solid fa-trash-can icon-delete"></i>
-                            <i onClick={() => selectUser(user)} class="fa-solid fa-pen icon-pen"></i>
+                            <i onClick={() => deleteUser(user.id)} className="fa-solid fa-trash-can icon-delete"></i>
+                            <i onClick={() => selectUser(user)} className="fa-solid fa-pen icon-pen"></i>
                         </div>
                     </div>
                 ))
